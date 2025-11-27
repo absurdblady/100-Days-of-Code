8cd3f01c89304b366dcc250a94b1d6b567b75744
Pawan Chaudhary <pawanchaudhary2061@gmail.com>
2025-11-27 00:09:51 -0600

    Day 1 Project - Band Name Generator

---
+++ Day 1 Project - Band Name Generator/task.py
@@ -0,0 +1,13 @@
+# print to the console
+print("Hello World!")
+
+# string concatenation
+# print("Hello" + " " + "World!")
+
+# input function : input("What is your name? ")
+# storing the input information to a variable
+name = input("What is your name? ")
+print("Hi " + name)
+
+# length of the input variable
+print("The length of your name " + name + " is " + str(len(name)))
