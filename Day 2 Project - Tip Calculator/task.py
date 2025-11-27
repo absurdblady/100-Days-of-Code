a6eef5076b1c54f756b02f4b203c4a03ea376b3f
Pawan Chaudhary <pawanchaudhary2061@gmail.com>
2025-11-27 13:04:08 -0600

    Day 2 Project - Tip Calculator

---
+++ Day 2 Project - Tip Calculator/task.py
@@ -0,0 +1,22 @@
+# subscripting
+print("Hello"[0])
+
+# integer is a whole number
+print(123+456)
+
+# large integers
+print(123_456_789)
+
+# float = floating point number
+print(3.14159)
+
+# boolean
+print(True)
+print(False)
+
+# printing the type
+print(type(123_456))
+
+bmi = 7.322324
+print(round(bmi))
+print(round(bmi,2))
